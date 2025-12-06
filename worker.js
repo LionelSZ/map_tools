5270848056c91a4c69f150e3870337bdaa417f87
LIonelSuZhe <2627470028@qq.com>
2025-12-07 03:27:52 +0800

    feat: 简化 worker.js 中的静态资源服务逻辑，并更新 wrangler.toml 以使用新的资产配置。

--- worker.js
+++ worker.js
@@ -1,14 +1,9 @@
 export default {
-    async fetch(request, env) {
-        const url = new URL(request.url);
-
-        // 访问静态文件时直接返回
-        const ext = url.pathname.split(".").pop();
-        if (["js", "css", "html", "svg", "png", "ico", "json", "jpg", "txt"].includes(ext)) {
-            return env.ASSETS.fetch(request);
+    async fetch(request, env, ctx) {
+        try {
+            return await env.ASSETS.fetch(request);
+        } catch (err) {
+            return await env.ASSETS.fetch("index.html");
         }
-
-        // 其他路径回退到 index.html (SPA 必须)
-        return env.ASSETS.fetch(new Request(`${url.origin}/index.html`, request));
-    },
-};
+    }
+}
